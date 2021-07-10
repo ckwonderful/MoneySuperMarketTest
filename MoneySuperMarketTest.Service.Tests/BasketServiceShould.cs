44a165831ba34defb431bd3bd908252005174a02
Carol Kelly <carol_irish@hotmail.com>
2021-07-10 09:42:24 +0100

    add failing test for percentage offer

--- MoneySuperMarketTest.Service.Tests/BasketServiceShould.cs
+++ MoneySuperMarketTest.Service.Tests/BasketServiceShould.cs
@@ -9,20 +9,41 @@ namespace MoneySuperMarketTest.Service.Tests
     [TestFixture]
     public class BasketServiceShould
     {
+        private Product _bread = new Product {Name = "bread", Cost = 1};
+        private Product _butter = new Product { Name = "butter", Cost = 0.8m };
+        private Product _milk = new Product { Name = "milk", Cost = 1.15m };
+
         [Test]
         public void CalculateTheCorrectTotalWhenThereAreNoOffersApplicable()
         {
             var products = new List<Product>
             {
-                new Product {Name = "bread", Cost = 1},
-                new Product {Name = "butter", Cost = 0.8m},
-                new Product {Name = "milk", Cost = 1.15m}
+                _bread,
+                _butter,
+                _milk
             };
 
             var sut = new BasketService();
             var result = sut.CalculateTotal(products);
 
             result.Should().Be(2.95m);
+        }
+
+        [Test]
+        public void CalculateTheCorrectTotalWhenThereIsAPercentageOffOffer()
+        {
+            var products = new List<Product>
+            {
+                _butter,
+                _butter,
+                _bread,
+                _bread
+            };
+
+            var sut = new BasketService();
+            var result = sut.CalculateTotal(products);
+
+            result.Should().Be(3.1m);
         }
     }
 
