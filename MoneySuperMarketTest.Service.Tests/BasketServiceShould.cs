6700cb3a409a72f73d0a344979dc40350b0545bc
Carol Kelly <carol_irish@hotmail.com>
2021-07-10 09:36:43 +0100

    first scenario test - failing

---
+++ MoneySuperMarketTest.Service.Tests/BasketServiceShould.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MoneySuperMarketTest.Service.Tests
+{
+    [TestFixture]
+    public class BasketServiceShould
+    {
+        [Test]
+        public void CalculateTheCorrectTotalWhenThereAreNoOffersApplicable()
+        {
+            var products = new List<Product>
+            {
+                new Product {Name = "bread", Cost = 1},
+                new Product {Name = "butter", Cost = 0.8m},
+                new Product {Name = "milk", Cost = 1.15m}
+            };
+
+            var sut = new BasketService();
+            var result = sut.CalculateTotal(products);
+
+            result.Should().Be(2.95m);
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+    public class BasketService
+    {
+        public decimal CalculateTotal(List<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
